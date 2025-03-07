40320534b06f80a624ce04725d46ea838434ffcd
Dhanjit Chamuah <dhanjitchamuah@Dhanjits-MacBook-Pro.local>
2025-03-07 13:50:25 +0530

    feature 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>hello</h1>
       <h1>feature 1 added</h1>
+      <h1>feature 2 added.. prevent direct push to main</h1>
     </div>
   );
 }
